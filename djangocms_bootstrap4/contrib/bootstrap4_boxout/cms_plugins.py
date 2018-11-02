dadb95f03285b96352558655a1cdcdbcb1c577fe
Oliver Ashford <oliver@compoundpartners.co.uk>
2018-11-02 15:52:15 +0000

    GAT01-155 > added collapse to be allowed in boxout

--- djangocms_bootstrap4/contrib/bootstrap4_boxout/cms_plugins.py
+++ djangocms_bootstrap4/contrib/bootstrap4_boxout/cms_plugins.py
@@ -23,7 +23,7 @@ class Bootstrap4BoxoutPlugin(CMSPluginBase):
     render_template = 'djangocms_bootstrap4/boxout.html'
     change_form_template = 'djangocms_bootstrap4/admin/boxout.html'
     allow_children = True
-    child_classes = ['TextPlugin', 'Bootstrap4LinkPlugin', 'Bootstrap4PicturePlugin']
+    child_classes = ['TextPlugin', 'Bootstrap4LinkPlugin', 'Bootstrap4PicturePlugin', 'BootstrapCollapsePlugin']
 
     fieldsets = [
         (None, {
