5a0a821ec1c1c71062bfdb46fc0e9cb2b1e3f3f5
Evgeny Dmitriev <evgeny.dmi3ev@gmail.com>
2019-07-31 13:15:05 +0200

    BWD-551 fix Error 500 trying to save plugin

--- djangocms_bootstrap4/contrib/bootstrap4_picture/models.py
+++ djangocms_bootstrap4/contrib/bootstrap4_picture/models.py
@@ -3,7 +3,7 @@ from __future__ import unicode_literals
 
 from django.db import models
 from django.utils.encoding import python_2_unicode_compatible
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import ugettext, ugettext_lazy as _
 from django.core.exceptions import ValidationError
 
 from cms.models import CMSPlugin
